a293b93599d3dcd1db8a6d49d776941d6d4c8e8e
KuyaYow <yuansabellina@gmail.com>
2024-07-19 22:02:48 +0800

    Add files via upload

---
+++ JsInfoSummaries.js
@@ -0,0 +1,11 @@
+"use strict";
+function myContact() {
+  alert(
+    "Contact no. (+63) 995 028 9948 \nEmail address: Yuansabellina@gmail.com"
+  );
+}
+function about() {
+  alert(
+    "This is a website that features summaries from the book 'Javascript.info'"
+  );
+}
